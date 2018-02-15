3d7225bd6c5fce3cace2d96863114c200b51609b
Unknown <yeisoncifuentes2@gmail.com>
2018-02-15 16:43:38 -0500

    crear variables
    declar las variables que se van a utilizar

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main{
+
+	public static void main(String[] arg){
+		System.out.println("");
+		System.out.println("clase de programacion II");
+		System.out.println("EJERCICIO DE NUMERO MAYOR O IGUAL");
+		System.out.println("");
+		System.out.println("\n\n\t\tEjercicio N 1");
+		System.out.println("");
+		System.out.println("");
+		int var1 = 21;
+		int var2 = 21;
+		
+		
+		
+			
+	}
+}
